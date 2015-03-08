7897511002d6d9945f037883189320bc4d1039ac
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-03-07 17:09:21 -0800

    quick fix for colorize bug

--- lib/printer.rb
+++ lib/printer.rb
@@ -12,8 +12,8 @@ module Printer
     constant << ROW_HORIZONTAL_MINOR_BORDER
   end
 
-  def self.print_horizontal_border(row, cell_size, major_or_minor = nil)
-    major_or_minor = self.set_horizontal_border(major_or_minor)
+  def self.get_horizontal_border(row, cell_size, major_or_minor = nil)
+    major_or_minor = self.border_major_or_minor(major_or_minor)
 
     border = []
     row.each do |val|
@@ -24,7 +24,7 @@ module Printer
     border.join(ROW_INTRA_CELL_BORDER)
   end
 
-  def self.print_row_contents(row, cell_size)
+  def self.get_row_contents(row, cell_size)
     contents = []
     row.each do |val|
       if val == :black || val == :white
@@ -38,20 +38,69 @@ module Printer
     contents.join(ROW_INTRA_CELL_BORDER)
   end
 
-  def self.print_whole_row(row, cell_size, major_or_minor = nil)
-    major_or_minor = self.set_horizontal_border(major_or_minor)
+  # wraps two strings into a 2-item array
+  def self.frame_whole_row(row, cell_size, major_or_minor = nil)
+    major_or_minor = self.border_major_or_minor(major_or_minor)
     output = []
-    output << self.print_horizontal_border(row, cell_size, major_or_minor)
-    output << self.print_row_contents(row, cell_size)
+    output << self.get_horizontal_border(row, cell_size, major_or_minor)
+    output << self.get_row_contents(row, cell_size)
     output
+  end
+
+  def self.corresponding_rows(row_from_b1, row_from_b2)
+    output = []
+    spacer = '    '
+
+    border = ''
+    border << self.get_horizontal_border(row_from_b1, 3)
+    border << spacer
+    border << self.get_horizontal_border(row_from_b2, 1)
+    output.push(border)
+
+    contents = ''
+    b1_contents = self.get_row_contents(row_from_b1, 3)
+    contents << b1_contents
+    contents << spacer
+    b2_contents = self.get_row_contents(row_from_b2, 1)
+    contents << b2_contents
+    output.push(contents)
+
+    output
+  end
+  def self.concat_two_boards(options)
+    b1 = options[:guess_board]
+    b2 = options[:hint_board]
+    b1_size = options[:b1_size] || 3
+    b2_size = options[:b2_size] || 1
+    spacer = self.initiate_cell(4)
+
+    new_board = []
+    b1.each_with_index do |row, ind|
+      border =  self.get_horizontal_border(b1[ind], b1_size)
+      border << spacer
+      border << self.get_horizontal_border(b2[ind], b2_size)
+      new_board.push(border)
+
+      contents =  self.get_row_contents(b1[ind], b1_size)
+      contents << spacer
+      contents =  self.get_row_contents(b2[ind], b2_size)
+      new_board.push(contents)
+    end
+    new_board
   end
 
   def self.print_whole_board(rows, cell_size)
     # print top row
-    puts self.print_whole_row(rows[0], cell_size, :major)
+    puts self.frame_whole_row(rows[0], cell_size, :major)
     # print rows
     rows[1..rows.length].each do |row|
-      puts self.print_whole_row(row, cell_size)
+      puts self.frame_whole_row(row, cell_size)
+    end
+  end
+
+  def self.print_two_boards(board)
+    board.each do |row|
+      puts row
     end
   end
 
@@ -60,7 +109,7 @@ module Printer
     contents.center(size)
   end
 
-  def self.set_horizontal_border(major_or_minor = nil)
+  def self.border_major_or_minor(major_or_minor = nil)
     case major_or_minor
     when :major
       major_or_minor = ROW_HORIZONTAL_MAJOR_BORDER
